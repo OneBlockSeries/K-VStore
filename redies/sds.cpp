f7548051b1eaa6c87676d7b119fa6837a05b4d19
xufeng <xufengyongyou@163.com>
2019-06-23 09:27:41 +0800

    完善sds基本操作

--- redies/sds.cpp
+++ redies/sds.cpp
@@ -1,2 +1,44 @@
 
 #include"sds.h"
+
+sds::sds(){
+
+    data=new char[10+1]; //默认开始是10，
+    data[10]='\0';         //方便使用一些字符操作函数       
+    len=0;
+    free=10;
+}
+sds::sds(const char* wdata){
+
+    int wlen=strlen(wdata);
+    if(wlen>10){
+        data=new char[wlen+1+10];
+        memcpy(data,wdata,slen);
+        data[wlen]='\0'
+        free=10
+        len=wlen
+    }else{
+
+        data=new char[10+1];
+        memcpy(data,wdata,wlen);
+        data[wlen]='\0';
+        free=10-wlen;
+        len=wlen;
+    }
+
+}
+sds::~sds(){
+
+    delete []data;
+}
+const char* sds::getdata(){
+
+    return data;
+}
+const int sds::getusedlen(){
+
+    return len;
+}
+const int sds::getfreelen(){
+    return free;
+}
--- redies/sds.h
+++ redies/sds.h
@@ -1,14 +1,17 @@
 
-class sds{
 
+//sds  simple dynamic string  简单动态字符串，用预 分配方式来减少内存频繁分配
+//sds二进制安全，一般的char* 遇到’\0‘则停止，因此不能存错二进制文件。
+class sds{
 public:
     sds();
     ~sds();
-    sds(const char*);
-    char* getdata();
-    int  getdatalen();
+    sds(const char* wdata);
+    const char* getdata();
+    const int  getusedlen();
+    const int  getfreelen();
 private:
     char* data;
     int len;        //已使用长度
-    int free;       //空闲长度
-}
+    int free;       //空闲长度   //总长度=len+free
+};
